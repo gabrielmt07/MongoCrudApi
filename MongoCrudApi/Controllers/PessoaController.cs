cf49e706e55993770cb062921a1020134a79262a
unknown <gabriel_motta07@yahoo.com.br>
2021-09-15 19:27:19 -0300

    logs configurados

--- MongoCrudApi/Controllers/PessoaController.cs
+++ MongoCrudApi/Controllers/PessoaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MongoCrudApi.Data.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -13,17 +14,20 @@ namespace MongoCrudApi.Controllers
     public class PessoaController : ControllerBase
     {
         private readonly IPessoaRepository _pessoaRepository;
+        private readonly ILogger _logger;
 
 
-        public PessoaController(IPessoaRepository pessoaRepository)
+        public PessoaController(IPessoaRepository pessoaRepository, ILogger<PessoaController> logger)
         {
             _pessoaRepository = pessoaRepository;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetPessoas()
         {
             var listaPessoas = await _pessoaRepository.Get();
+            _logger.LogInformation("Lista de pessoas retornado");
             return Ok(listaPessoas);
         }
 
@@ -31,6 +35,7 @@ namespace MongoCrudApi.Controllers
         public async Task<IActionResult> GetPessoaPorNome(string id)
         {
             var pessoa = await _pessoaRepository.GetPessoaByName(id);
+            _logger.LogInformation("Cadastro encontrado");
             return Ok(pessoa);
         }
 
@@ -38,6 +43,7 @@ namespace MongoCrudApi.Controllers
         public async Task<ActionResult> InserirPessoa(Pessoa pessoa)
         {
             await _pessoaRepository.Inserir(pessoa);
+            _logger.LogInformation("Pessoa inserida");
 
             return Ok(pessoa);
         }
@@ -46,6 +52,7 @@ namespace MongoCrudApi.Controllers
         public async Task<ActionResult> AtualizarPessoa(string id, Pessoa pessoa)
         {
             await _pessoaRepository.Update(id, pessoa);
+            _logger.LogInformation("Cadastro atualizado");
 
             return Ok();
         }
@@ -54,6 +61,7 @@ namespace MongoCrudApi.Controllers
         public async Task<ActionResult> ApagarPessoa(string id)
         {
             await _pessoaRepository.Delete(id);
+            _logger.LogInformation("Cadastro deletado");
 
             return Ok();
         }
